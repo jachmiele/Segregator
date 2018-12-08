15d979711024ca5d02f65150f7498b94b9dd8afa
jachmiele <jakubgry@gmail.com>
2018-12-08 14:29:37 +0100

    Pierwsza wersja gry

---
+++ app/src/main/java/pl/cba/jchmielewski/segregator/result.java
@@ -0,0 +1,59 @@
+package pl.cba.jchmielewski.segregator;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView scoreLabel = findViewById(R.id.scoreLabel);
+        TextView highScoreLabel = findViewById(R.id.highScore);
+
+        int score = getIntent().getIntExtra("SCORE", 0);
+        scoreLabel.setText(score + "");
+
+        SharedPreferences settings = getSharedPreferences("GAME_DATA", Context.MODE_PRIVATE);
+        int highScore = settings.getInt("HIGH_SCORE", 0);
+
+        if (score > highScore){
+            highScoreLabel.setText("High Score : "+ score);
+
+            //save
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("HIGH_SCORE", score);
+            editor.commit();
+        }else {
+            highScoreLabel.setText("High score : "+ highScore);
+        }
+
+    }
+
+    public void tryAgain(View view){
+        //sprzekierowanie po kliknieciu try again
+        startActivity(new Intent(getApplicationContext(), start.class));
+    }
+
+    // Disable return button
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event){
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN){
+            switch (event.getKeyCode()){
+                case KeyEvent.KEYCODE_BACK:
+                    return true;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+}
